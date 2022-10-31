ba6ad2b80d56e2e224e51a393294863d84e82275
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-10-31 20:13:58 +0000

    cambios en home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -32,14 +32,42 @@ export const Home = () => {
         </div>
       </div>
       <section className="bg-primary text-light p-5 bg-opacity-75">
-        <div className="Container">
+        {/* <div className="Container">
           <div className="d-md-flex justify-content-between align-items-center">
             <h3 className="mb-3.mb-md-0 text-center"> Nuestros viajes destacados </h3>
             <Viajes />
           </div>
         </div>
       </section>
+	  <div className="row row-cols-1 row-cols-md-2 g-4">
+	  	<div className="col">
+		<div className="card">
+		  <img src="https://imgur.com/aJjTFph.jpg" className="card-img-top" alt="..."/>
+			<div className="card-body">
+		  		<h5 className="card-title"> Pasajeros </h5>
+		  		<p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+		</div>
+		</div>
+		</div>
+		
+		<div className="row row-cols-1 row-cols-md-2 g-4">
+	  	<div className="col">
+		<div className="card">
+		  <img src="https://imgur.com/S8vYnVH.jpg" className="card-img-top" alt="..."/>
+			<div className="card-body">
+		  		<h5 className="card-title"> Conductores </h5>
+		  		<p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+		</div>
+		</div>
+		</div>
+  			 */}
+			 
     </section>
+	
+	
+	
 
     // </div>
   );

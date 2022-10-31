414089818665275f0387731a00bbcce5dee3cefc
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-10-31 20:30:58 +0000

    agregue dos cards

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -32,16 +32,19 @@ export const Home = () => {
         </div>
       </div>
       <section className="bg-primary text-light p-5 bg-opacity-75">
-        {/* <div className="Container">
+        <div className="Container">
           <div className="d-md-flex justify-content-between align-items-center">
             <h3 className="mb-3.mb-md-0 text-center"> Nuestros viajes destacados </h3>
             <Viajes />
           </div>
         </div>
       </section>
-	  <div className="row row-cols-1 row-cols-md-2 g-4">
+	  
+	  <div className="container d-flex justify-content-center">
+
+	  <div className="row row-cols-1 row-cols-md-2 g-4 m-4">
 	  	<div className="col">
-		<div className="card">
+		<div className="card" style={{width: "28rem"}}>
 		  <img src="https://imgur.com/aJjTFph.jpg" className="card-img-top" alt="..."/>
 			<div className="card-body">
 		  		<h5 className="card-title"> Pasajeros </h5>
@@ -51,9 +54,9 @@ export const Home = () => {
 		</div>
 		</div>
 		
-		<div className="row row-cols-1 row-cols-md-2 g-4">
+		<div className="row row-cols-1 row-cols-md-2 g-4 m-4">
 	  	<div className="col">
-		<div className="card">
+		<div className="card" style={{width: "28rem"}}>
 		  <img src="https://imgur.com/S8vYnVH.jpg" className="card-img-top" alt="..."/>
 			<div className="card-body">
 		  		<h5 className="card-title"> Conductores </h5>
@@ -62,7 +65,12 @@ export const Home = () => {
 		</div>
 		</div>
 		</div>
-  			 */}
+
+
+	  </div>
+
+	  
+  			
 			 
     </section>
 	

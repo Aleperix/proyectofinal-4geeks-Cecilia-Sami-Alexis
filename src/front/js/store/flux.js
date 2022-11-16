176d575d6c66506c161fc02046cf735039a0e567
Aleperix <aleperixx@gmail.com>
2022-11-16 15:31:30 +0000

    Agregados cambios back y front de la vista de viaje individual (Cambios hechos por Sami)

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,27 +6,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 			siteName: "Fromtony",
 			usuario: {},
 			auth: false,
-			departamentos: [
-				{ departamento: "Artigas", img: "https://i.imgur.com/OLPdj9C.jpg" },
-				{ departamento: "Canelones", img: "https://i.imgur.com/g1wxlPz.jpg" },
-				{ departamento: "Cerro Largo", img: "https://i.imgur.com/FT3QlRn.jpg" },
-				{ departamento: "Colonia", img: "https://i.imgur.com/L6wPfHm.jpg" },
-				{ departamento: "Durazno", img: "https://i.imgur.com/yUUPUj0.jpg" },
-				{ departamento: "Flores", img: "https://i.imgur.com/cW5cFNb.jpg" },
-				{ departamento: "Florida", img: "https://i.imgur.com/00I9zth.jpg" },
-				{ departamento: "Lavalleja", img: "https://i.imgur.com/tNHD1YT.jpg" },
-				{ departamento: "Maldonado", img: "https://i.imgur.com/11CVMt6.jpg" },
-				{ departamento: "Montevideo", img: "https://i.imgur.com/HaPN1S9.jpg" },
-				{ departamento: "Paysandú", img: "https://i.imgur.com/ito9Lzs.jpg" },
-				{ departamento: "Río Negro", img: "https://i.imgur.com/wmUI8nr.jpg" },
-				{ departamento: "Rivera", img: "https://i.imgur.com/lVi9wR4.jpg" },
-				{ departamento: "Rocha", img: "https://i.imgur.com/HCYKfww.jpg" },
-				{ departamento: "Salto", img: "https://i.imgur.com/3UG4SJV.jpg" },
-				{ departamento: "San José", img: "https://i.imgur.com/zmUa0ZN.jpg" },
-				{ departamento: "Soriano", img: "https://i.imgur.com/sXAMQWi.jpg" },
-				{ departamento: "Tacuarembó", img: "https://i.imgur.com/npRwouy.jpg" },
-				{ departamento: "Treinta y Tres", img: "https://i.imgur.com/fQIj3Yz.jpg" },
-			],
 			viajes: [],
 		},
 		actions: {
@@ -88,7 +67,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			//Inicio Usuarios
 			login: async (value) => {
 				const action = getActions();
-				let response = await action.postData(process.env.API_URL + "/api/login", value);
+				let response = await action.postData(process.env.BACKEND_URL + "/api/login", value);
 				if (!response.hasOwnProperty("code")) {
 					localStorage.setItem("token", response.data.access_token);
 					setStore({ auth: true });
@@ -106,7 +85,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			isAuth: async () => {
 				const action = getActions();
-				let response = await action.getData(process.env.API_URL + "/api/isauth", { Authorization: "Bearer " + localStorage.getItem("token") });
+				let response = await action.getData(process.env.BACKEND_URL + "/api/isauth", { Authorization: "Bearer " + localStorage.getItem("token") });
 				if (!response.hasOwnProperty("code")) {
 					setStore({ auth: true });
 					setStore({ usuario: response.data.usuario });
@@ -117,7 +96,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			getProfile: async (id) => {
 				const action = getActions();
-				let response = await action.getData(process.env.API_URL + "/api/profile/" + id, { Authorization: "Bearer " + localStorage.getItem("token") });
+				let response = await action.getData(process.env.BACKEND_URL + "/api/profile/" + id, { Authorization: "Bearer " + localStorage.getItem("token") });
 				if (!response.hasOwnProperty("code")) {
 					return response.data;
 				}
@@ -128,7 +107,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			//Inicio Viajes
 			getAllTravels: async () => {
 				const action = getActions();
-				const response = await action.getData(process.env.API_URL + "/api/viajes");
+				const response = await action.getData(process.env.BACKEND_URL + "/api/viajes");
 				if (!response.hasOwnProperty("code")) {
 					setStore({ viajes: response.data });
 					return response.data;
@@ -138,7 +117,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			postTravel: async (value) => {
 				console.log(value);
 				const action = getActions();
-				let response = await action.postData(process.env.API_URL + "/api/viajes/new", value, { Authorization: "Bearer " + localStorage.getItem("token") });
+				let response = await action.postData(process.env.BACKEND_URL + "/api/viajes/new", value, { Authorization: "Bearer " + localStorage.getItem("token") });
 				if (!response.hasOwnProperty("code")) {
 					action.getAllTravels()
 					return true;
@@ -147,6 +126,22 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log(response);
 					return {message: response.data.message}
 				}
+			},
+			getOneTravel: async (id) => {
+				const action = getActions();
+				const response = await action.getData(process.env.BACKEND_URL + "/api/viaje/"+ id);
+				if (!response.hasOwnProperty("code")) {
+					return response.data;
+				}
+				return response.response;
+			},
+			modifyTravel: async (id,data) => {
+				const action = getActions();
+				const response = await action.putData(process.env.BACKEND_URL + "/api/viaje/"+ id, data, { Authorization: "Bearer " + localStorage.getItem("token") });
+				if (!response.hasOwnProperty("code")) {
+					return response.data;
+				}
+				return response.response;
 			},
 			//Fin Viajes
 
